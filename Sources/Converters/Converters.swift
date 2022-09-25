f9195f0ee4ad49f299ad797f58a29dcbbd27e9b7
Henry Jao <jaohaohsuan@gmail.com>
2022-09-25 17:29:38 +1300

    Add implementation code

--- Sources/Converters/Converters.swift
+++ Sources/Converters/Converters.swift
@@ -1,6 +1,19 @@
-public struct Converters {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
+public struct Converters {
+    
+    func convertNZDtoUSD(nzd: String) -> String {
+        let usdRate = 0.59
+        let nzdValue = Double(nzd) ?? 0
+
+        if nzdValue <= 0 {
+            return "Please enter a positive number."
+        }
+
+        if nzdValue >= 1_000_000 {
+            return "Value too big to convert!"
+        }
+
+        return "$\(String(format: "%.2f", nzdValue * usdRate))"
     }
 }
--- Tests/ConvertersTests/ConvertersTests.swift
+++ Tests/ConvertersTests/ConvertersTests.swift
@@ -2,10 +2,22 @@ import XCTest
 @testable import Converters
 
 final class ConvertersTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Converters().text, "Hello, World!")
+    func test_convert100_return59() {
+        // sut means system under testing
+        let sut = Converters()
+
+        let actual = sut.convertNZDtoUSD(nzd: "100")
+        let expected = "$59.00"
+
+        XCTAssertEqual(actual, expected)
+    }
+
+    func test_convertNeg100_returnsErrorMessage() {
+        let sut = Converters()
+
+        let actual = sut.convertNZDtoUSD(nzd: "-100")
+        let expected = "Please enter a positive number."
+
+        XCTAssertEqual(actual, expected)
     }
 }
